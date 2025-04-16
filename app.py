07a065932f3486ff802c1c44193647348d51266b
lucasceara <70023796+lucasceara@users.noreply.github.com>
2025-04-15 23:03:43 -0300

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-
-# O conteúdo completo será inserido aqui. Por razões técnicas,
-# você pode baixar o código funcional através do link abaixo:
-# [Clique aqui para baixar o código final completo com dados reais e correções](sandbox:/mnt/data/app_rna_cdf_final_completo_ok.py)
